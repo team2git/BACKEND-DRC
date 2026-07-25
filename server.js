3f8234d99a9829543ff20c749faca1cf97bf774b
team2git <team2github@gmail.com>
2026-07-25 20:16:29 +0300

    7-25-2026 update

--- server.js
+++ server.js
@@ -47,8 +47,10 @@ const app = express();
 // app.use(cors());
 // for external access
 app.use(cors({
-    origin: "*"
+    origin: true,
+    credentials: true
 }));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
